6ef67f3764fd0959edcb0dc0ee06ca64e0503f9f
Isabella <isabellafj97@gmail.com>
2017-06-28 10:14:41 -0600

    Fix readFile test, cleanup

--- src/lecture/FileTest.java
+++ src/lecture/FileTest.java
@@ -1,33 +1,41 @@
 package lecture;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class FileTest {
-	private static final String DIR = "C:\\Users\\isabe\\Documents\\compsci\\Final\\ATM\\src\\lecture\\test.txt";
+	private static String DIR = "";
 
 	public static void main(String[] args) {
-		
-		// Attempt to read file
 		try {
+			// https://stackoverflow.com/questions/1131500/using-relative-directory-path-in-java
+
+			DIR = new File("./src/lecture/test.txt").getCanonicalPath();
+			System.out.println(DIR);
+			
+			// https://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java
+
 			Scanner in = new Scanner(new FileReader(DIR));
 			
+			// Add each line to the StringBuilder
 			StringBuilder sb = new StringBuilder();
-			while(in.hasNext()) {
-			    sb.append(in.next()).append("\n");
-			}
-			in.close();
-			String outString = sb.toString();
+			while(in.hasNext()) sb.append(in.next()).append("\n");
+
+			in.close(); // Make sure to close the scanner
+			String outString = sb.toString(); // StringBuilders *aren't* strings themselves
 
 			System.out.println(outString);
 
-		} catch (FileNotFoundException e) {
+		} catch (FileNotFoundException notFound) {
+			// https://stackoverflow.com/questions/21197737/how-to-print-an-error-message-to-standard-error
+			
+			System.err.println("Could not find the file specified.");
+		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.print("Could not locate the file specified.");
-		}	
+		}
 
 	}
 }
